40b16741ce3cc8f2daa55531b5585e828d8039e2
ixboy <ismaelixboy@gmail.com>
2021-09-22 19:58:48 +0100

    add entry js file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
 import './style.scss';
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-unused-vars */
+const list = document.querySelector('#list');
+const toDoList = [
+  {
+    description: 'Eat breakfast today',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'watch video tutorials',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'attend microverse classes',
+    completed: false,
+    index: 2,
+  },
+];
+
+function addItems() {
+  list.innerHTML = '';
+  toDoList.forEach((e) => {
+    list.innerHTML += `<li>
+    <a class="text"> ${e.description} </a>
+    <i class="de fa fa-trash"></i>
+  </li>`;
+  });
+}
+window.addEventListener('DOMContentLoaded', addItems());
