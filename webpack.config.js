a677fd720b591178978f9d41aaea08a52d7b0cef
ixboy <ismaelixboy@gmail.com>
2021-09-22 20:24:25 +0100

    fix linters

--- webpack.config.js
+++ webpack.config.js
@@ -34,8 +34,8 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: ["babel-loader"]
-      }
+        use: ['babel-loader'],
+      },
     ],
   },
 };
