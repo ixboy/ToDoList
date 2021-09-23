a54850d30c1e4b8ebd591d95af2d66702e5f3c24
ixboy <ismaelixboy@gmail.com>
2021-09-23 02:13:12 +0100

    edit src files / changed all codes

--- src/index.js
+++ src/index.js
@@ -9,32 +9,3 @@ import '@fortawesome/fontawesome-free/js/brands';
 import './style.scss';
 import './main.js';
 
-const list = document.querySelector('#list');
-const toDoList = [
-  {
-    description: 'Eat breakfast today',
-    completed: false,
-    index: 0,
-  },
-  {
-    description: 'watch video tutorials',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'attend microverse classes',
-    completed: false,
-    index: 2,
-  },
-];
-
-function addItems() {
-  list.innerHTML = '';
-  toDoList.forEach((e) => {
-    list.innerHTML += `<li>
-    <a class="text"> ${e.description} </a>
-    <i class="de fa fa-trash"></i>
-  </li>`;
-  });
-}
-window.addEventListener('DOMContentLoaded', addItems());
