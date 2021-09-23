a8fb9c3fd04c9ab6b12141165ad96a26d60229fb
ixboy <ismaelixboy@gmail.com>
2021-09-23 02:48:34 +0100

    add addTodo function

--- src/index.js
+++ src/index.js
@@ -9,3 +9,29 @@ import '@fortawesome/fontawesome-free/js/brands';
 import './style.scss';
 import './main.js';
 
+const todoForm = document.querySelector('.todo-form');
+const todoInput = document.querySelector('.todo-input');
+const todoItemsList = document.querySelector('.todo-items');
+
+const todos = [];
+
+function addTodo(item) {
+  // if item is not empty
+  if (item !== '') {
+    const todo = {
+      id: Date.now(),
+      description: item,
+      completed: false,
+      index: todos.length + 1,
+    };
+
+    todos.push(todo);
+    renderTodos(todos); // then renders them between <ul>
+    todoInput.value = '';
+  }
+}
+
+todoForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  addTodo(todoInput.value);
+});
