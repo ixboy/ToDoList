dac6c1fcb6c69e0531bacdbb9848439409d01088
ixboy <ismaelixboy@gmail.com>
2021-09-25 01:43:18 +0100

    remove comments and fixed class

--- src/main.js
+++ src/main.js
@@ -4,29 +4,23 @@ import { todoItemsList, todos, updateLocalStorage } from './index.js';
 
 class App { 
   static renderTodos(todos) {
-    // clear everything inside <ul> with class=todo-items
     todoItemsList.innerHTML = '';
     todos.forEach((item) => {
       // check if the item is completed
       const checked = item.completed ? 'checked' : null;
   
       const li = document.createElement('li');
-      // <li class="item"> </li>
       li.setAttribute('class', 'item');
       // <li class="item" data-key="20200708"> </li>
       li.setAttribute('data-key', item.id);
-      /* <li class="item" data-key="20200708">
-            <input type="checkbox" class="checkbox">
-            <input type="text" class="input-desc"/>
-            <button class="delete-button">X</button>
-          </li> */
+   
       if (item.completed === true) {
         li.classList.add('checked');
       }
   
       li.innerHTML = `
         <input type="checkbox" class="checkbox" ${checked}>
-        <input type="text" value="${item.description}" class="input-desc">
+        <input type="text" value="${item.description}" class="input-desc ${checked}">
         <button class="delete-button">X</button>
       `;
       todoItemsList.append(li);
